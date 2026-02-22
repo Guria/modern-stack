710b3e4d6aa07a1d825079fdbb470e76ae2e3479
artalar <artalar@ya.ru>
2026-02-22 11:56:06 +0300

    fix: move connectLogger to the init
    * It is important to call connectLogger before any other code, to be able to hook app atoms

--- src/reatom.init.ts
+++ src/reatom.init.ts
@@ -1,4 +1,11 @@
-import { clearStack } from '@reatom/core'
+import { clearStack, connectLogger, context } from '@reatom/core'
 
 // Don't dare to remove this line!
 clearStack()
+
+export const rootFrame = context.start()
+
+console.log('import.meta.env', import.meta.env)
+if (import.meta.env['DEV']) {
+	rootFrame.run(connectLogger)
+}
