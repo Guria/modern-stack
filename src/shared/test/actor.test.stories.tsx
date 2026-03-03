ee8a8a3d3fbadf2a78fbcc021495fe8818f3dfe2
Aleksei Gurianov <gurianov@gmail.com>
2026-03-03 13:25:32 +0300

    test: add nested scope() test

--- src/shared/test/actor.test.stories.tsx
+++ src/shared/test/actor.test.stories.tsx
@@ -41,3 +41,23 @@ ScopeBasic.test('finds elements within scoped parent', async () => {
 	// After scope - searches from root again
 	await I.see(heading('Page Title'))
 })
+
+export const ScopeNested = meta.story({ name: 'scope() nested scopes' })
+
+ScopeNested.test('nested scopes resolve relative to outer scope', async () => {
+	await I.scope(role('main'), async () => {
+		// Outer scope: searches within main
+		await I.see(heading('Section Heading'))
+
+		await I.scope(role('article'), async () => {
+			// Inner scope: searches within article (which is within main)
+			await I.see(heading('Article Heading'))
+			await I.see(text('Article content'))
+			// Should NOT find section content
+			await I.dontSee(text('Section content'))
+		})
+
+		// Back to outer scope: can see section content again
+		await I.see(text('Section content'))
+	})
+})
