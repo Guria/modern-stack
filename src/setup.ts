1a215d2e9ec0d8b02ac858a8888358d200d07562
Aleksei Gurianov <gurianov@gmail.com>
2026-05-31 15:18:48 +0300

    feat: enable VITE_CONNECT_LOGGER for improved logging during builds and tests

--- src/setup.ts
+++ src/setup.ts
@@ -1,10 +1,13 @@
-import { clearStack, context } from '@reatom/core'
+import { clearStack, connectLogger, context } from '@reatom/core'
 
 // Don't dare to remove this line!
 clearStack()
 
 export const rootFrame = context.start()
 
-if (import.meta.env['VITE_CONNECT_LOGGER'] === 'true') {
-	// rootFrame.run(connectLogger)
+if (
+	import.meta.env['VITE_CONNECT_LOGGER'] === 'true' ||
+	(import.meta.env['DEV'] && import.meta.env['VITE_CONNECT_LOGGER'] !== 'false')
+) {
+	rootFrame.run(connectLogger)
 }
